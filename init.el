cad157230c4d782bd3ed3e73028aab10fa8e8e42
Jason Heeris <jason.heeris@nal.gov.au>
2017-10-19 15:13:21 +1100

    Confine customisation to magic.el.

--- init.el
+++ init.el
@@ -1,24 +1,7 @@
+(package-initialize)
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-; (package-initialize)
+(setq custom-file (expand-file-name "magic.el" user-emacs-directory))
+(load custom-file t t)
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
-
 (load-library "customisations")
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Envy Code R" :foundry "unknown" :slant normal :weight normal :height 143 :width normal)))))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (base16-theme magit use-package))))
