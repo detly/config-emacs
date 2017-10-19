3c1721cb046a38eb396711bd3d9d206229ab000b
detly <jason.heeris@gmail.com>
2017-10-20 06:55:49 +1100

    Separate font for modeline and main buffer.

--- lisp/theming.el
+++ lisp/theming.el
@@ -1,3 +1,19 @@
+;; Default font.
+(set-face-attribute 'default nil
+    :family "Envy Code R"
+    :height 160
+    :width 'normal
+    :weight 'normal
+)
+
+;; Modeline font.
+(set-face-attribute 'mode-line nil
+    :family "terminus"
+    :height 120
+    :width 'normal
+    :weight 'normal
+)
+
 (use-package base16-theme
   :if (display-graphic-p)
   :config (
