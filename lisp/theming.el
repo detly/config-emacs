554595c3fdb7f1f53b1d5a70d948247630c994c9
Jason Heeris <jason.heeris@nal.gov.au>
2017-10-20 12:04:38 +1100

    Fixed modeline formatting.

--- lisp/theming.el
+++ lisp/theming.el
@@ -1,9 +1,32 @@
+;; Modeline font.
+(defun theme-modeline (modeline)
+  "Apply theming to modelines."
+  (set-face-attribute modeline nil
+      :family "terminus"
+      :height 120
+      :width 'normal
+      :weight 'normal
+  )
+)
+
+(defun theme-all-the-modelines ()
+  (mapc 'theme-modeline '(
+      mode-line
+      mode-line-buffer-id
+      mode-line-emphasis
+      mode-line-highlight
+      mode-line-inactive
+    )
+  )
+)
+
 (use-package base16-theme
   :if (display-graphic-p)
   :config (
     progn
       (load-theme 'base16-default-dark t)
       (set-cursor-color "dark grey")
+      (theme-all-the-modelines)
   )
 )
 
@@ -15,16 +38,7 @@
 ;; Default font.
 (set-face-attribute 'default nil
     :family "Envy Code R"
-    :height 160
+    :height 140
     :width 'normal
     :weight 'normal
 )
-
-;; Modeline font.
-(set-face-attribute 'mode-line nil
-    :family "terminus"
-    :height 120
-    :width 'normal
-    :weight 'normal
-)
-
