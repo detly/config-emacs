8428f4053fd4fb343bf0014e16ee11d6187210cb
Jason Heeris <jason.heeris@nal.gov.au>
2017-10-20 14:20:51 +1100

    Enable fuzzy searching.

--- lisp/projects.el
+++ lisp/projects.el
@@ -1,4 +1,15 @@
-(use-package flx-ido)
+(use-package flx-ido
+  :config (
+    progn
+      (ido-mode 1)
+      (ido-everywhere 1)
+      (flx-ido-mode 1)
+  )
+)
+
+(use-package ido-vertical-mode
+  :config (ido-vertical-mode 1)
+)
 
 (use-package projectile
   :config (
